5cc03f685347ea11cea409d1ccb761f57385ea33
Jsong <365556835@qq.com>
2023-06-19 11:47:59 +0800

    调用地图接口获取路径规划信息

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/AmapConfigConstants.java
@@ -0,0 +1,24 @@
+package com.mashibing.internalcommon.constant;
+
+/*
+ *@author js
+ *@version 1.0
+ */
+public class AmapConfigConstants {
+
+    public static final String DIRECTION_URL = "https://restapi.amap.com/v3/direction/driving";
+
+
+    /*
+    * 路径规划json key值
+    * */
+
+    public static  final  String STATUS = "status";
+
+    public static  final  String ROUTE = "route";
+
+    public static  final  String PATHS = "paths";
+
+    public static  final  String DISTANCE = "distance";
+    public static  final  String DURATION = "duration";
+}
--- service-map/src/main/java/com/mashibing/servicemap/ServiceMapApplication.java
+++ service-map/src/main/java/com/mashibing/servicemap/ServiceMapApplication.java
@@ -2,6 +2,8 @@ package com.mashibing.servicemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /*
  *@author js
@@ -11,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceMapApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceMapApplication.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
     }
 }
