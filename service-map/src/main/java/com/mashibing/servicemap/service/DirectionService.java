6709fc46a80d1cf85f3da5c3c0a038e2eeb31acf
Jsong <365556835@qq.com>
2023-06-19 11:44:51 +0800

    调用地图接口获取路径规划信息

--- service-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
+++ service-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
@@ -2,9 +2,12 @@ package com.mashibing.servicemap.service;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.DirectionResponse;
+import com.mashibing.servicemap.remote.MapDirectionClient;
 import com.sun.org.apache.regexp.internal.RE;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.pattern.PathPattern;
 
 /*
  *@author js
@@ -13,14 +16,18 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class DirectionService {
+    @Autowired
+    private MapDirectionClient mapDirectionClient;
 
     /*
     * 根据起点经纬度和终点经纬度获取距离(米)和时长(分钟)
     * */
     public ResponseResult driving(String depLongitude, String depLatitude, String destLongitude, String destLatitude){
-        DirectionResponse directionResponse = new DirectionResponse();
-        directionResponse.setDistance(123);
-        directionResponse.setDuration(11);
-        return ResponseResult.success(directionResponse);
+
+        //调用第三方地图接口
+        DirectionResponse direction = mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
+
+
+        return ResponseResult.success(direction);
     }
 }
