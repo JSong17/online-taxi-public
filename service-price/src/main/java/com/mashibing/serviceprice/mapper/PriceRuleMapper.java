a1a999b85b9c714c2e0992a72e1aa100b05f798c
Jsong <365556835@qq.com>
2023-06-20 18:22:05 +0800

    读取计价规则

---
+++ service-price/src/main/java/com/mashibing/serviceprice/mapper/PriceRuleMapper.java
@@ -0,0 +1,13 @@
+package com.mashibing.serviceprice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mashibing.internalcommon.dto.PriceRule;
+import org.springframework.stereotype.Repository;
+
+/*
+ *@author js
+ *@version 1.0
+ */
+@Repository
+public interface PriceRuleMapper  extends BaseMapper<PriceRule> {
+}
