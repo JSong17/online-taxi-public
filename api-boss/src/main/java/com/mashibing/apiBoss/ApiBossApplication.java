4b94da1fe062865b9e9ef449f11fd75e8cf614eb
Jsong <365556835@qq.com>
2023-06-23 17:57:02 +0800

    api-boss调用service-driver-user完成插入司机的调用

--- api-boss/src/main/java/com/mashibing/apiBoss/ApiBossApplication.java
+++ api-boss/src/main/java/com/mashibing/apiBoss/ApiBossApplication.java
@@ -2,12 +2,14 @@ package com.mashibing.apiBoss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
  *@author js
  *@version 1.0
  */
 @SpringBootApplication
+@EnableFeignClients
 public class ApiBossApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiBossApplication.class);
