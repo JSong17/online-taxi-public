38f0f0ccbe9586215b8da3e86cf5fd6f2eaf2cb5
Jsong <365556835@qq.com>
2023-06-23 15:58:06 +0800

    service-driver-user 注册到nacos

--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/ServiceDriverUserApplication.java
@@ -3,12 +3,14 @@ package com.mashibing.serviceDriverUser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /*
  *@author js
  *@version 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.mashibing.serviceDriverUser.mapper")
 public class ServiceDriverUserApplication {
     public static void main(String[] args) {
