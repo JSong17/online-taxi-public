1005197a86b58676be5f43542f9f6be7ff4a8e93
Jsong <365556835@qq.com>
2023-07-03 16:11:56 +0800

    并发集群测试环境准备,及错误演示

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.mashibing.apipassenger.controller;
 
+import com.mashibing.apipassenger.remote.ServiceOrderClient;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -33,6 +36,16 @@ public class TestController {
     @GetMapping("/noauthTest")
     public ResponseResult noauthTest(){
         return  ResponseResult.success("noauth test");
+    }
+
+    @Autowired
+    ServiceOrderClient serviceOrderClient;
+
+    @GetMapping("/test-real-time-order/{orderId}")
+    public String dispatchRealTimeOrder(@PathVariable("orderId") long orderId) {
+        System.out.println("并发测试：orderId：" + orderId);
+        serviceOrderClient.dispatchRealTimeOrder(orderId);
+        return "test-real-time-order success";
     }
 
 
