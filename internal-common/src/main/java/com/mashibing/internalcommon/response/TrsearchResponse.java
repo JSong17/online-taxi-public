26a49ce42f16b2e847565b1864fb02804e40969a
Jsong <365556835@qq.com>
2023-07-05 18:19:11 +0800

    解析轨迹查询结果

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/response/TrsearchResponse.java
@@ -0,0 +1,14 @@
+package com.mashibing.internalcommon.response;
+
+import lombok.Data;
+
+/*
+ *@author js
+ *@version 1.0
+ */
+@Data
+public class TrsearchResponse {
+    private Long driveMile;
+
+    private Long driveTime;
+}
--- service-map/src/main/java/com/mashibing/servicemap/controller/TerminalController.java
+++ service-map/src/main/java/com/mashibing/servicemap/controller/TerminalController.java
@@ -2,6 +2,7 @@ package com.mashibing.servicemap.controller;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.TerminalResponse;
+import com.mashibing.internalcommon.response.TrsearchResponse;
 import com.mashibing.servicemap.service.TerminalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,18 +22,37 @@ public class TerminalController {
     @Autowired
     private TerminalService terminalService;
 
+    /**
+     * 添加终端
+     * @param name
+     * @param desc
+     * @return
+     */
     @PostMapping("/add")
     public ResponseResult<TerminalResponse> add(String name, String desc){
         return terminalService.add(name,desc);
     }
 
+    /**
+     * 终端搜索
+     * @param center
+     * @param radius
+     * @return
+     */
     @PostMapping("/aroundsearch")
     public ResponseResult<List<TerminalResponse>> aroundsearch(String center, Integer radius){
         return terminalService.aroundsearch(center,radius);
     }
 
+    /**
+     * 查询轨迹距离
+     * @param tid
+     * @param starttime
+     * @param endtime
+     * @return
+     */
     @PostMapping("/trsearch")
-    public ResponseResult trsearch(String tid,Long starttime,Long endtime){
+    public ResponseResult<TrsearchResponse> trsearch(String tid, Long starttime, Long endtime){
         return terminalService.trsearch(tid,starttime,endtime);
     }
 
