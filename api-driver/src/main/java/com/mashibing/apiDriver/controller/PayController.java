d7ef4bac959ead2add0e6ea6dbbc3ae37424d8c8
Jsong <365556835@qq.com>
2023-07-06 23:37:18 +0800

    司机发起收款代码编写完成

--- api-driver/src/main/java/com/mashibing/apiDriver/controller/PayController.java
+++ api-driver/src/main/java/com/mashibing/apiDriver/controller/PayController.java
@@ -1,6 +1,8 @@
 package com.mashibing.apiDriver.controller;
 
+import com.mashibing.apiDriver.service.PayService;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pay")
 public class PayController {
 
+    @Autowired
+    PayService payService;
     /**
      * 司机发起收款
      * @param orderId
@@ -24,6 +28,6 @@ public class PayController {
     @PostMapping("/push-pay-info")
     public ResponseResult pushPayInfo(@RequestParam String orderId,@RequestParam String price,@RequestParam Long passengerId){
 
-        return null;
+        return payService.pushPayInfo(orderId,price,passengerId);
     }
 }
--- api-driver/src/main/java/com/mashibing/apiDriver/service/PayService.java
+++ api-driver/src/main/java/com/mashibing/apiDriver/service/PayService.java
@@ -1,6 +1,10 @@
 package com.mashibing.apiDriver.service;
 
+import com.mashibing.apiDriver.remote.ServiceSsePushClient;
+import com.mashibing.internalcommon.constant.IdentityConstants;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /*
@@ -10,11 +14,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class PayService {
 
+    @Autowired
+    ServiceSsePushClient serviceSsePushClient;
+
     public ResponseResult pushPayInfo(String orderId,String price,Long passengerId){
         //封装信息
-
+        JSONObject message = new JSONObject();
+        message.put("price",price);
         //推送信息
-
+        serviceSsePushClient.push(passengerId, IdentityConstants.PASSENGER_IDENTITY, message.toString());
         return ResponseResult.success();
     }
 }
